050ea3e724be7f138af0f8c9e0cb5c58c46a0e80
Kazuki Aoshima <aocm.dev@gmail.com>
2021-03-17 21:46:01 +0900

    Refactor: 不要なテストの除去

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,7 +7,6 @@ describe('Fizz Buzz 数列と変換規則を扱うfizzbuzzクラス', () => {
   })
 
   describe('数を文字列に変換する', () => {
-
     describe('3の倍数のときは数の代わりに「Fizz」に変換する', () => {
       test('3を渡すと文字列"Fizz"を返す', () => {
         // 実行 & 検証
@@ -26,11 +25,6 @@ describe('Fizz Buzz 数列と変換規則を扱うfizzbuzzクラス', () => {
       test('1を渡すと文字列"1"を返す', () => {
         // 実行 & 検証
         expect(fizzbuzz.convert(1)).toBe("1");
-      });
-
-      test('2を渡すと文字列"2"を返す', () => {
-        // 実行 & 検証
-        expect(fizzbuzz.convert(2)).toBe("2");
       });
     })
   })
