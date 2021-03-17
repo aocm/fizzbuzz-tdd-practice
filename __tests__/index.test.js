ce8e87ae33a7309e2d8ee0a5bc35687474750e5e
Kazuki Aoshima <aocm.dev@gmail.com>
2021-03-17 20:40:48 +0900

    Refactor
      beforeEach( () => {
        // 前準備処理をここにまとめる
        console.log("前準備")
      })
    としたとき、以下になる。必ず順番通りに行われると思われる。
    ```
     PASS  __tests__/index.test.js
      FizzBuzzTest
        √ 1を渡すと文字列"1"を返す (21 ms)
        √ 2を渡すと文字列"2"を返す (2 ms)
        √ 3を渡すと文字列"Fizz"を返す (1 ms)
    
      console.log
        前準備
    
          at Object.beforeEach (__tests__/index.test.js:7:13)
    
      console.log
        1
    
          at Object.test (__tests__/index.test.js:12:13)
    
      console.log
        前準備
    
          at Object.beforeEach (__tests__/index.test.js:7:13)
    
      console.log
        2
    
          at Object.test (__tests__/index.test.js:18:13)
    
      console.log
        前準備
    
          at Object.beforeEach (__tests__/index.test.js:7:13)
    
      console.log
        3
    
          at Object.test (__tests__/index.test.js:24:13)
    
    Test Suites: 1 passed, 1 total
    Tests:       3 passed, 3 total
    Snapshots:   0 total
    Time:        1.708 s
    Ran all test suites.
    ```

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,19 +2,21 @@ import { fizzbuzz } from '../index';
 
 
 describe('FizzBuzzTest', () => {
+  beforeEach( () => {
+    // 前準備処理をここにまとめる
+  })
+
   test('1を渡すと文字列"1"を返す', () => {
-    // 準備
     // 実行 & 検証
     expect(fizzbuzz.convert(1)).toBe("1");
   });
+
   test('2を渡すと文字列"2"を返す', () => {
-    // 準備
     // 実行 & 検証
     expect(fizzbuzz.convert(2)).toBe("2");
   });
 
   test('3を渡すと文字列"Fizz"を返す', () => {
-    // 準備
     // 実行 & 検証
     expect(fizzbuzz.convert(3)).toBe("Fizz");
   });
