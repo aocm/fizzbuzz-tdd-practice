72feda715ed183d071c2bada0612b17d6acb6cf1
Kazuki Aoshima <aocm.dev@gmail.com>
2021-03-17 20:42:59 +0900

    Red

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -20,4 +20,9 @@ describe('FizzBuzzTest', () => {
     // 実行 & 検証
     expect(fizzbuzz.convert(3)).toBe("Fizz");
   });
+
+  test('5を渡すと文字列"Buzz"を返す', () => {
+    // 実行 & 検証
+    expect(fizzbuzz.convert(5)).toBe("Buzz");
+  });
 });
