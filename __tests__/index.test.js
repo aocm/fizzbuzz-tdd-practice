622016b742f639fae98979de9294d96de9fa9bb5
Kazuki Aoshima <aocm.dev@gmail.com>
2021-03-17 20:17:36 +0900

    Refactor

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,9 +4,7 @@ import { fizzbuzz } from '../index';
 describe('FizzBuzzTest', () => {
   test('1を渡すと文字列"1"を返す', () => {
     // 準備
-    // 実行
-    // 検証
-    const actual = fizzbuzz.convert(1);
-    expect(actual).toBe("1");
+    // 実行 & 検証
+    expect(fizzbuzz.convert(1)).toBe("1");
   });
 });
