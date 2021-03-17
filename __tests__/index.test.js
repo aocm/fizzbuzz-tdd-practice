586627770bdec21256de1ae80a6d024ad3bbd4c6
Kazuki Aoshima <aocm.dev@gmail.com>
2021-03-17 20:59:12 +0900

    Refactor: テストをドメイン知識を含むドキュメントにする
    ```
    PASS  __tests__/index.test.js
      Fizz Buzz 数列と変換規則を扱うfizzbuzzクラス
        数を文字列に変換する
          3の倍数のときは数の代わりに「Fizz」に変換する
            √ 3を渡すと文字列"Fizz"を返す (2 ms)
          5の倍数のときは数の代わりに「Buzz」に変換する
            √ 5を渡すと文字列"Buzz"を返す
          そのほかの数にの時はそのまま出力する
            √ 1を渡すと文字列"1"を返す
            √ 2を渡すと文字列"2"を返す
    
    Test Suites: 1 passed, 1 total
    Tests:       4 passed, 4 total
    Snapshots:   0 total
    Time:        1.704 s
    Ran all test suites.
    ```

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,34 +1,37 @@
 import { fizzbuzz } from '../index';
 
 
-describe('FizzBuzzTest', () => {
+describe('Fizz Buzz 数列と変換規則を扱うfizzbuzzクラス', () => {
   beforeEach( () => {
     // 前準備処理をここにまとめる
   })
 
   describe('数を文字列に変換する', () => {
-    test('1を渡すと文字列"1"を返す', () => {
-      // 実行 & 検証
-      expect(fizzbuzz.convert(1)).toBe("1");
-    });
 
-    test('2を渡すと文字列"2"を返す', () => {
-      // 実行 & 検証
-      expect(fizzbuzz.convert(2)).toBe("2");
-    });
-  })
-  
-  describe('3の倍数のときは数の代わりに「Fizz」に変換する', () => {
-    test('3を渡すと文字列"Fizz"を返す', () => {
-      // 実行 & 検証
-      expect(fizzbuzz.convert(3)).toBe("Fizz");
-    });
-  })
+    describe('3の倍数のときは数の代わりに「Fizz」に変換する', () => {
+      test('3を渡すと文字列"Fizz"を返す', () => {
+        // 実行 & 検証
+        expect(fizzbuzz.convert(3)).toBe("Fizz");
+      });
+    })
 
-  describe('5の倍数のときは数の代わりに「Buzz」に変換する', () => {
-    test('5を渡すと文字列"Buzz"を返す', () => {
-      // 実行 & 検証
-      expect(fizzbuzz.convert(5)).toBe("Buzz");
-    });
+    describe('5の倍数のときは数の代わりに「Buzz」に変換する', () => {
+      test('5を渡すと文字列"Buzz"を返す', () => {
+        // 実行 & 検証
+        expect(fizzbuzz.convert(5)).toBe("Buzz");
+      });
+    })
+
+    describe('そのほかの数にの時はそのまま出力する', () => {
+      test('1を渡すと文字列"1"を返す', () => {
+        // 実行 & 検証
+        expect(fizzbuzz.convert(1)).toBe("1");
+      });
+
+      test('2を渡すと文字列"2"を返す', () => {
+        // 実行 & 検証
+        expect(fizzbuzz.convert(2)).toBe("2");
+      });
+    })
   })
 });
